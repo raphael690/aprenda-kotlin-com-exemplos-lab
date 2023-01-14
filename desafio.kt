84a0cbeef3ef4e691a0e011aefa5d8567205fa63
raphael690 <raphael.tecnologiainformacaorj@gmail.com>
2023-01-14 14:49:31 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,39 +1,64 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+enum class Nivel { BASIC, INTERMEDIARIO, AVANCADO }
 
- class Usuario(vararg nome: String) {
+ class Usuario(val nome: String, val email: String, val telefone: String ) {
 
     }
 
-class ConteudoEducacional(vararg nome: String, var duracao: Any)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 
-data class Formacao(var nome: Usuario, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>(nome)
+    val inscritos = mutableListOf<Usuario>()
 
     fun matricular(usuario: Usuario) {
 
         inscritos.add(usuario)
     }
 
+
+
     fun mostrarinscritos() {
-        inscritos.forEach { println(nome) }
+        for (usuarios in inscritos) { 
+            println("Nome: ${usuarios.nome}" + "Email: ${usuarios.email}" + "Telefone: ${usuarios.telefone}" ) }
+
     }
 }
 
 fun main() {
-    val content = listOf(ConteudoEducacional("Curso de Kotlin", duracao = "40 Horas"))
+    // Simulações com objetos
+    val usuarioTest = Usuario(nome = "Gil Pina, " , email = "gilpina@gmail.com, " , telefone = "21999663322")
+    val segundoUsuarioTest = Usuario(nome = "Nanda Pina, ", email = "nandapina@gmail.com, ", telefone = "28911224455")
+    val terceiroUsuarioTest = Usuario(nome = "Cola Pina, ", email = "colapina@gmail.com, ", telefone = "11922778800")
 
-    val users = Usuario("Joao")
+    val conteudosTest: List<ConteudoEducacional> = listOf(
+        ConteudoEducacional(
+        nome = "Java",
+        duracao = 340),
+        ConteudoEducacional(
+            nome = "Kotlin",
+            duracao = 120),
+        ConteudoEducacional(
+            nome = "Python",
+            duracao = 224
+        )
+    )
 
+    val formacaoTest = Formacao(
+        nome = "Linguagem de programacao Java, Kotlin e Python",
+        conteudos = conteudosTest,
+        nivel =  Nivel.AVANCADO
+    )
 
-    val formacao = Formacao(users, content)
-    formacao.matricular(users)
-    val inscr = (formacao.inscritos)
-
-    formacao.mostrarinscritos()
-
-
+    println("Quantidade de conteúdos: ${conteudosTest.size}")
+    println("Conteúdos: $conteudosTest")
+    println("Formação: ${formacaoTest.nome} (${formacaoTest.nivel})")
+    println("Inscritos: ${formacaoTest.inscritos.size}")
+    formacaoTest.matricular(usuarioTest)
+    formacaoTest.matricular(segundoUsuarioTest)
+    formacaoTest.matricular(terceiroUsuarioTest)
+    formacaoTest.mostrarinscritos()
+    println("Inscritos: ${formacaoTest.inscritos.size}")
 }
